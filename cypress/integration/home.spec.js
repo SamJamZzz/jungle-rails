9de663235b537cc6ffe88757ee4e23a5d75a492d
Sam <samjulian6@gmail.com>
2022-11-22 16:24:54 -0800

    Add tests to verify the home page is working properly through Cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,21 @@
+describe('home page of jungle-rails app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Visits the home page', () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+})
